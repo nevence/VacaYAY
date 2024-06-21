9067f07d22cc062690d15e319c0063e839213e38
nevence <nevenceminic@gmail.com>
2024-06-21 14:48:34 +0200

    fix: added view of remaining days off number

---
+++ VacaYAY/BusinessLogicLayer/ViewModel/UserDaysOffViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLogicLayer.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.ViewModel
+{
+    public class UserDaysOffViewComponent : ViewComponent
+    {
+        private readonly IServiceManager _service;
+
+        public UserDaysOffViewComponent(IServiceManager service)
+        {
+            _service = service;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var user = await _service.AuthService.GetUser(id);
+            return View(user.DaysOffNumber);
+        }
+    }
+}
